616e6d18f56cc259c40fbad692fd6e794d2ba17b
Kadim Gültekin <35685546+Arkenon@users.noreply.github.com>
2025-12-12 12:20:54 +0300

    Update license from GPL-2.0 to GPL-3.0

--- block-style-modifiers.php
+++ block-style-modifiers.php
@@ -5,8 +5,8 @@
  * Version: 0.1.0
  * Author: Kadim Gültekin
  * Author URI: https://kadimgultekin.com
- * License: GPL-2.0+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * License: GPL-3.0
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.txt
  * Text Domain: bsm
  */
 

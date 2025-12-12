b601f96357afc7658589fe925e3149ca5f47bd0d
Kadim Gültekin <35685546+Arkenon@users.noreply.github.com>
2025-12-12 12:14:57 +0300

    Create main php file

---
+++ block-style-modifiers.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Plugin Name: Block Style Modifiers
+ * Description: Adds additive, multi-select style modifiers to Gutenberg blocks.
+ * Version: 0.1.0
+ * Author: Kadim Gültekin
+ * Author URI: https://kadimgultekin.com
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: bsm
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/**
+ * Internal registry
+ */
+function bsm_get_style_modifier_registry() {
+    static $registry = [];
+
+    return $registry;
+}
+
+function register_block_style_modifier( string $block_name, array $modifier ) {
+    $registry = &bsm_get_style_modifier_registry();
+
+    if ( empty( $modifier['name'] ) || empty( $modifier['class'] ) ) {
+        return;
+    }
+
+    if ( ! isset( $registry[ $block_name ] ) ) {
+        $registry[ $block_name ] = [];
+    }
+
+    $registry[ $block_name ][ $modifier['name'] ] = wp_parse_args(
+        $modifier,
+        [
+            'label'        => $modifier['name'],
+            'description'  => '',
+            'category'     => '',
+            'inline_style' => '',
+        ]
+    );
+}
+
+function bsm_collect_inline_styles() {
+    $registry = bsm_get_style_modifier_registry();
+    $styles   = '';
+
+    foreach ( $registry as $block => $modifiers ) {
+        foreach ( $modifiers as $modifier ) {
+            if ( ! empty( $modifier['inline_style'] ) ) {
+                $styles .= "\n" . $modifier['inline_style'];
+            }
+        }
+    }
+
+    return trim( $styles );
+}
+
+
+function bsm_enqueue_editor_assets() {
+    wp_enqueue_script(
+        'block-style-modifiers-editor',
+        plugins_url( 'assets/editor.js', __FILE__ ),
+        [
+            'wp-blocks',
+            'wp-element',
+            'wp-components',
+            'wp-compose',
+            'wp-data',
+            'wp-hooks',
+            'wp-editor',
+        ],
+        '0.1.0',
+        true
+    );
+
+    // Dummy style handle
+    wp_register_style( 'block-style-modifiers-editor-style', false );
+    wp_enqueue_style( 'block-style-modifiers-editor-style' );
+
+    $inline_css = bsm_collect_inline_styles();
+    if ( $inline_css ) {
+        wp_add_inline_style(
+            'block-style-modifiers-editor-style',
+            $inline_css
+        );
+    }
+
+    wp_add_inline_script(
+        'block-style-modifiers-editor',
+        'window.__BLOCK_STYLE_MODIFIERS__ = ' . wp_json_encode(
+            bsm_get_style_modifier_registry()
+        ) . ';',
+        'before'
+    );
+}
+add_action( 'enqueue_block_editor_assets', 'bsm_enqueue_editor_assets' );
+
+function bsm_enqueue_frontend_styles() {
+    $inline_css = bsm_collect_inline_styles();
+    if ( ! $inline_css ) {
+        return;
+    }
+
+    wp_register_style( 'block-style-modifiers-style', false );
+    wp_enqueue_style( 'block-style-modifiers-style' );
+
+    wp_add_inline_style(
+        'block-style-modifiers-style',
+        $inline_css
+    );
+}
+add_action( 'wp_enqueue_scripts', 'bsm_enqueue_frontend_styles' );
